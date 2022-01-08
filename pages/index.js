383b182075819c0231b178ccdfde109f5c3d1a71
Pedro Araujo <pedrotcaraujo@gmail.com>
2022-01-08 00:32:54 +0000

    Add some beauty

--- pages/index.js
+++ pages/index.js
@@ -8,14 +8,20 @@ export default function Home() {
         <title>Pedro Araujo</title>
         <meta name="description" content="Pedro Araujo - Software Engineer" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=PT+Sans:400,400italic,700|Abril+Fatface"></link>
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
           Pedro Araujo
         </h1>
-        <p>Hey, I am a Software Engineer and this is my personal decentralized website</p>
+        <p className={styles.subtitle}>I am a software engineer who works with <b>JavaScript</b> and this is a <b>decentralized</b> website.</p>
       </main>
+      <footer className={styles.footer}>
+        <a href="https://www.linkedin.com/in/pedrotcaraujo/">linkedin</a>
+        <a href="https://github.com/pedrotcaraujo">github</a>
+        <a href="https://twitter.com/pedrotcaraujo">twitter</a>
+      </footer>
     </div>
   )
 }
